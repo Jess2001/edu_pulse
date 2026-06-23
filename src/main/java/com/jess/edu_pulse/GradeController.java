09aca1ad482bd74499046193d15462918b324645
Jess2001 <jacinta.wangui@kabarak.ac.ke>
2026-06-23 15:03:07 +0300

    ch:add student and grades data

--- src/main/java/com/jess/edu_pulse/GradeController.java
+++ src/main/java/com/jess/edu_pulse/GradeController.java
@@ -10,15 +10,54 @@ import java.util.stream.Collectors;
 public class GradeController {
 
     private List<Grade> grades = Arrays.asList(
-        new Grade(1L, 1L, 1L, "Mathematics", 78, "B"),
+        // Form 1 - Amina
+        new Grade(1L, 1L, 1L, "Mathematics", 78, "B+"),
         new Grade(2L, 1L, 2L, "English", 85, "A"),
         new Grade(3L, 1L, 3L, "Biology", 60, "C"),
+        // Form 1 - Brian
         new Grade(4L, 2L, 1L, "Mathematics", 90, "A"),
-        new Grade(5L, 2L, 2L, "English", 72, "B"),
-        new Grade(6L, 3L, 1L, "Mathematics", 55, "D"),
-        new Grade(7L, 3L, 3L, "Biology", 88, "A"),
-        new Grade(8L, 4L, 4L, "Chemistry", 91, "A"),
-        new Grade(9L, 4L, 5L, "History", 67, "C")
+        new Grade(5L, 2L, 2L, "English", 72, "B+"),
+        new Grade(6L, 2L, 3L, "Biology", 88, "A"),
+        // Form 1 - Cynthia
+        new Grade(7L, 3L, 1L, "Mathematics", 55, "C+"),
+        new Grade(8L, 3L, 2L, "English", 68, "B"),
+        new Grade(9L, 3L, 3L, "Biology", 74, "B+"),
+        // Form 2 - David
+        new Grade(10L, 4L, 1L, "Mathematics", 91, "A"),
+        new Grade(11L, 4L, 4L, "Chemistry", 67, "B"),
+        new Grade(12L, 4L, 5L, "History", 80, "A"),
+        // Form 2 - Esther
+        new Grade(13L, 5L, 1L, "Mathematics", 76, "B+"),
+        new Grade(14L, 5L, 2L, "English", 83, "A"),
+        new Grade(15L, 5L, 3L, "Biology", 70, "B+"),
+        // Form 2 - Felix
+        new Grade(16L, 6L, 1L, "Mathematics", 62, "C+"),
+        new Grade(17L, 6L, 4L, "Chemistry", 58, "C+"),
+        new Grade(18L, 6L, 5L, "History", 74, "B+"),
+        // Form 3 - Grace
+        new Grade(19L, 7L, 1L, "Mathematics", 88, "A"),
+        new Grade(20L, 7L, 2L, "English", 92, "A"),
+        new Grade(21L, 7L, 3L, "Biology", 79, "B+"),
+        // Form 3 - Hassan
+        new Grade(22L, 8L, 1L, "Mathematics", 65, "B"),
+        new Grade(23L, 8L, 4L, "Chemistry", 71, "B+"),
+        new Grade(24L, 8L, 5L, "History", 60, "C+"),
+        // Form 3 - Irene
+        new Grade(25L, 9L, 1L, "Mathematics", 77, "B+"),
+        new Grade(26L, 9L, 2L, "English", 84, "A"),
+        new Grade(27L, 9L, 3L, "Biology", 69, "B"),
+        // Form 4 - James
+        new Grade(28L, 10L, 1L, "Mathematics", 94, "A"),
+        new Grade(29L, 10L, 4L, "Chemistry", 88, "A"),
+        new Grade(30L, 10L, 5L, "History", 76, "B+"),
+        // Form 4 - Karen
+        new Grade(31L, 11L, 1L, "Mathematics", 70, "B+"),
+        new Grade(32L, 11L, 2L, "English", 78, "B+"),
+        new Grade(33L, 11L, 3L, "Biology", 85, "A"),
+        // Form 4 - Liam
+        new Grade(34L, 12L, 1L, "Mathematics", 63, "C+"),
+        new Grade(35L, 12L, 4L, "Chemistry", 72, "B+"),
+        new Grade(36L, 12L, 5L, "History", 68, "B")
     );
 
     @GetMapping
--- src/main/java/com/jess/edu_pulse/StudentController.java
+++ src/main/java/com/jess/edu_pulse/StudentController.java
@@ -15,9 +15,17 @@ public class StudentController {
     public List<Student> getAllStudents() {
         return Arrays.asList(
             new Student(1L, "Amina Wanjiru", "Form 1", "East"),
-            new Student(2L, "Brian Otieno", "Form 2", "West"),
-            new Student(3L, "Cynthia Achieng", "Form 3", "East"),
-            new Student(4L, "David Kamau", "Form 4", "West")
+            new Student(2L, "Brian Otieno", "Form 1", "West"),
+            new Student(3L, "Cynthia Achieng", "Form 1", "East"),
+            new Student(4L, "David Kamau", "Form 2", "West"),
+            new Student(5L, "Esther Njeri", "Form 2", "East"),
+            new Student(6L, "Felix Omondi", "Form 2", "West"),
+            new Student(7L, "Grace Waweru", "Form 3", "East"),
+            new Student(8L, "Hassan Mwangi", "Form 3", "West"),
+            new Student(9L, "Irene Chebet", "Form 3", "East"),
+            new Student(10L, "James Kipchoge", "Form 4", "West"),
+            new Student(11L, "Karen Auma", "Form 4", "East"),
+            new Student(12L, "Liam Mutua", "Form 4", "West")
         );
     }
 }
